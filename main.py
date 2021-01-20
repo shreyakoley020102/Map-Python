a5592f3b675460ce6c76b86fb9433dd053cfc68d
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-01-20 21:57:45 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import folium
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
+
+map.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am Marker", icon=folium.Icon(color='green')))
+
 map.save("Map1.html")
 #map1 = folium.Map(location=[38.58, -99.09],zoom_start=6)
 #map1.save("Map2.html")
