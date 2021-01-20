048af9219e2d3634b4492f6c36f2987e16fe96ec
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-01-20 22:23:45 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import folium
+import pandas
+
+data=pandas.read_csv("Volcanoes.txt")
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 #map.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am Marker", icon=folium.Icon(color='green')))
@@ -7,6 +11,7 @@ fg = folium.FeatureGroup(name="My Map")
 
 for coordinates in [[38.2, -99.1],[36.2, -98.1]]:
     fg.add_child(folium.Marker(location=coordinates, popup="Hi I am Marker", icon=folium.Icon(color='green')))
+
 #fg.add_child(folium.Marker(location=[37.2, -97.1], popup="Hi I am Marker", icon=folium.Icon(color='green')))
 
 map.add_child(fg)
