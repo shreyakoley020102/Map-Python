6fc2a7ab31cdfae3c9f56db2265a8c1ed7141260
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-01-20 22:31:08 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[38.2, -99.1],[36.2, -98.1]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="Hi I am Marker", icon=folium.Icon(color='green')))
+for lt, ln in zip(lat,lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am Marker", icon=folium.Icon(color='green')))
 
 #fg.add_child(folium.Marker(location=[37.2, -97.1], popup="Hi I am Marker", icon=folium.Icon(color='green')))
 
