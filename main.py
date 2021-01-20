70379d32cbeb5a3733bdab5a219fb44acaea34b7
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-01-20 22:26:24 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import pandas
 
 data=pandas.read_csv("Volcanoes.txt")
 
+lon=list(data["LON"])
+lat=list(data["LAT"])
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 #map.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am Marker", icon=folium.Icon(color='green')))
