35d6f178ae8f75833cb20052531522d89cdede52
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-01-20 21:51:53 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import folium
-map = folium.Map(location=[38.58, -99.09])
+map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 map.save("Map1.html")
-map1 = folium.Map(location=[38.58, -99.09], zoom_start=6)
-map1.save("Map2.html")
+#map1 = folium.Map(location=[38.58, -99.09],zoom_start=6)
+#map1.save("Map2.html")
 
 #tiles = "Stamen Terrain"
 
